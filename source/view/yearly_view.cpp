d453ec26f1400780cfa1a1a5965bac67d4f3491f
Nikola Ducak <nikola.ducak@gmail.com>
2022-06-10 21:22:28 +0200

    refactor: clean up YearView initialization

--- source/view/yearly_view.cpp
+++ source/view/yearly_view.cpp
@@ -5,30 +5,18 @@
 #include "ftxui/dom/elements.hpp"
 #include "ftxui/dom/flexbox_config.hpp"
 #include "ftxui_ext/extended_containers.hpp"
+#include "highlight_menu.hpp"
 
 #include <ftxui/screen/terminal.hpp>
 #include <sstream>
 
 namespace clog::view {
 YearView::YearView(const model::Date& today) :
-    m_screen { ScreenInteractive::Fullscreen() },
-    m_calendarButtons { Calendar::make(today, makeCalendarOptions(today)) },
-    m_tagsMenu { WindowedMenu::make("Tags", &m_tags,
-                                    MenuOption { .on_change =
-                                                     [this] {
-                                                         m_handler->handleInputEvent(
-                                                             { UIEvent::FOCUSED_TAG_CHANGE,
-                                                               m_tagsMenu->selected() });
-                                                     } }) },
-    m_sectionsMenu { WindowedMenu::make(
-        "Sections", &m_sections,
-        MenuOption { .on_change =
-                         [this] {
-                             m_handler->handleInputEvent(
-                                 { UIEvent::FOCUSED_SECTION_CHANGE,
-                                   m_sectionsMenu->selected() });
-                         } }) },
-    m_rootComponent { makeFullUIComponent() } {}
+    m_screen { ScreenInteractive::Fullscreen() }, m_calendarButtons { Calendar::make(
+                                                      today, makeCalendarOptions(today)) },
+    m_tagsMenu { makeSectionsMenu() }, m_sectionsMenu { makeSectionsMenu() }, m_rootComponent {
+        makeFullUIComponent()
+    } {}
 
 void YearView::run() { m_screen.Loop(m_rootComponent); }
 
@@ -46,22 +34,23 @@ void YearView::setPreviewString(const std::string& string) {
 }
 
 std::shared_ptr<Promptable> YearView::makeFullUIComponent() {
-    
     auto container = ftxui_ext::CustomContainer(
         {
             m_tagsMenu,
             m_sectionsMenu,
             m_calendarButtons,
-        }, Event::Tab, Event::TabReverse);
+        },
+        Event::Tab, Event::TabReverse);
 
     auto whole_ui_renderer = Renderer(container, [this, container] {
         std::stringstream date;
         date << "Today: " << model::Date::getToday().formatToString("%d. %m. %Y.");
-        // preview window can sometimes be wider than the menus & calendar, it's simpler to keep them 
-        // centered while the preview window changes and stretches this vbox container than to keep the 
-        // preview window size fixed
-        return vbox(text(date.str()) | center, 
-                container->Render() | center, m_logFileContentsPreview) | center;
+        // preview window can sometimes be wider than the menus & calendar, it's simpler to keep
+        // them centered while the preview window changes and stretches this vbox container than to
+        // keep the preview window size fixed
+        return vbox(text(date.str()) | center, container->Render() | center,
+                    m_logFileContentsPreview) |
+               center;
     });
 
     auto event_handler = CatchEvent(whole_ui_renderer, [&](Event e) {
@@ -78,7 +67,7 @@ std::shared_ptr<Promptable> YearView::makeFullUIComponent() {
 
 CalendarOption YearView::makeCalendarOptions(const Date& today) {
     CalendarOption option;
-    option.transform = [this, today] (const auto& date, const auto& state) {
+    option.transform = [this, today](const auto& date, const auto& state) {
         auto element = text(state.label);
         if (state.focused)
             element = element | inverted;
@@ -90,7 +79,7 @@ CalendarOption YearView::makeCalendarOptions(const Date& today) {
             element = element | dim;
         return element | center;
     };
-    // TODO: Ignoring the provided new date only for the controller to ask 
+    // TODO: Ignoring the provided new date only for the controller to ask
     // for new date is ugly
     option.focusChange = [this](const auto& /* date */) {
         m_handler->handleInputEvent({ UIEvent::FOCUSED_DATE_CHANGE });
@@ -101,4 +90,18 @@ CalendarOption YearView::makeCalendarOptions(const Date& today) {
     return std::move(option);
 }
 
+std::shared_ptr<WindowedMenu> YearView::makeTagsMenu() {
+    MenuOption option { .on_change = [this] {
+        m_handler->handleInputEvent({ UIEvent::FOCUSED_TAG_CHANGE, m_tagsMenu->selected() });
+    } };
+    return WindowedMenu::make("Tags", &m_tags, option);
+}
+
+std::shared_ptr<WindowedMenu> YearView::makeSectionsMenu() {
+    MenuOption option = { .on_change = [this] {
+        m_handler->handleInputEvent(
+            { UIEvent::FOCUSED_SECTION_CHANGE, m_sectionsMenu->selected() });
+    } };
+    return WindowedMenu::make("Sections", &m_sections, option);
+}
 }  // namespace clog::view
--- source/view/yearly_view.hpp
+++ source/view/yearly_view.hpp
@@ -1,12 +1,10 @@
 #pragma once
 
-#include "year_view_base.hpp"
-
 #include "../model/date.hpp"
 #include "calendar_component.hpp"
 #include "highlight_menu.hpp"
 #include "promptable.hpp"
-
+#include "year_view_base.hpp"
 
 #include <array>
 #include <ftxui/component/captured_mouse.hpp>
@@ -39,34 +37,39 @@ public:
 
     void showCalendarForYear(unsigned year) override;
 
-    void prompt(std::string message, std::function<void()> onYesCallback)  override{
+    void prompt(std::string message, std::function<void()> onYesCallback) override {
         m_rootComponent->prompt(message, onYesCallback);
     }
 
-    int& selectedTag()  override{ return m_tagsMenu->selected(); }
-    int& selectedSection()  override{ return m_sectionsMenu->selected(); }
-    
-    void setInputHandler(InputHandlerBase* handler)  override{ m_handler = handler; }
+    int& selectedTag() override { return m_tagsMenu->selected(); }
+    int& selectedSection() override { return m_sectionsMenu->selected(); }
 
-    void setAvailableLogsMap(const model::YearMap<bool>* map)  override{ m_availabeLogsMap = map; }
-    void setHighlightedLogsMap(const model::YearMap<bool>* map)  override{ m_highlightedLogsMap = map; }
+    void setInputHandler(InputHandlerBase* handler) override { m_handler = handler; }
 
-    void setTagMenuItems(std::vector<std::string> items)  override{ m_tags = std::move(items); }
+    void setAvailableLogsMap(const model::YearMap<bool>* map) override { m_availabeLogsMap = map; }
+    void setHighlightedLogsMap(const model::YearMap<bool>* map) override {
+        m_highlightedLogsMap = map;
+    }
 
-    void setSectionMenuItems(std::vector<std::string> items)  override{ m_sections = std::move(items); }
+    void setTagMenuItems(std::vector<std::string> items) override { m_tags = std::move(items); }
+
+    void setSectionMenuItems(std::vector<std::string> items) override {
+        m_sections = std::move(items);
+    }
 
     void setPreviewString(const std::string& string) override;
-    
+
     // temporay restore terminal to its roriginal state
     // to executa a function. used to start the editor
-    void withRestoredIO(std::function<void()> func)  override{ m_screen.WithRestoredIO(func)(); }
+    void withRestoredIO(std::function<void()> func) override { m_screen.WithRestoredIO(func)(); }
 
-    model::Date getFocusedDate() const  override{ return m_calendarButtons->getFocusedDate(); }
+    model::Date getFocusedDate() const override { return m_calendarButtons->getFocusedDate(); }
 
 private:
-
     std::shared_ptr<Promptable> makeFullUIComponent();
     CalendarOption makeCalendarOptions(const Date& today);
+    std::shared_ptr<WindowedMenu> makeTagsMenu();
+    std::shared_ptr<WindowedMenu> makeSectionsMenu();
 };
 
 }  // namespace clog::view
